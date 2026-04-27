16ca5d45fe6bc016d2418c31934e528af41bfab5
TommasoBarazzetta <tommaso.barazzetta@mail.polimi.it>
2026-04-27 13:00:06 +0200

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -26,7 +26,16 @@ const config = {
 			paths: {
 				// SOSTITUISCI 'nome-repo' con il nome esatto della tua repository su GitHub
 				base: process.env.NODE_ENV === 'production' ? '/2026-02-sveltekit' : '',
-			}
+			},
+			// AGGIUNGI QUESTO BLOCCO:
+        prerender: {
+            handleHttpError: ({ path, referrer, message }) => {
+                // Ignora l'errore se Svelte non trova la radice assoluta
+                if (path === '/' || path === '/2026-02-sveltekit') return;
+                
+                // Altrimenti lancia l'errore normalmente
+                throw new Error(message);
+            }
 		}
 };
 
